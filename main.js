3bc530b9b991a5d87aaf2ed23f36732db4eaaed6
Egorych <mikhalevichegor@gmail.com>
2021-08-01 15:37:21 +0300

    added behavior for the minus button

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ const btnPlus = document.getElementById('btnPlus')
 btnPlus.addEventListener('click', function (){
     input.value = parseInt(input.value) +1
 })
+btnMinus.addEventListener('click', function (){
+    input.value = parseInt(input.value) -1
+})
+btnReset.addEventListener('click', function (){
+    input.value = parseInt(input.value) - 0
+})
