2adaf73509cd68be6a399d6094bce33f48381ab7
Egorych <mikhalevichegor@gmail.com>
2021-08-01 15:48:29 +0300

    we move the data into a separate variable - btnPlus

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-
 const input = document.getElementById('input')
 const btnMinus = document.getElementById('btnMinus')
 const btnReset = document.getElementById('btnReset')
 const btnPlus = document.getElementById('btnPlus')
 
-btnPlus.addEventListener('click', function (){
-    input.value = parseInt(input.value) +1
+let counter = 0
+input.couter = counter
+
+btnPlus.addEventListener('click', function () {
+    counter = counter + 1
+    input.value = counter
 })
-btnMinus.addEventListener('click', function (){
-    input.value = parseInt(input.value) -1
+btnMinus.addEventListener('click', function () {
+    input.value = parseInt(input.value) - 1
 })
-btnReset.addEventListener('click', function (){
+btnReset.addEventListener('click', function () {
     input.value = 0
 })
