593c72c3e09c1293f4b04cc8736fab20a05f5b12
Egorych <mikhalevichegor@gmail.com>
2021-08-01 15:30:25 +0300

    included the main.js file

---
+++ main.js
@@ -0,0 +1,5 @@
+
+const input = document.getElementById('input')
+const btnMinus = document.getElementById('btnMinus')
+const btnReset = document.getElementById('btnReset')
+const btnPlus = document.getElementById('btnPlus')
