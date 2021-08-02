514ee044be6fb6af39602b8ee8ad92dee3a695f6
Egorych <mikhalevichegor@gmail.com>
2021-08-02 23:13:20 +0300

    transferred data to controller.js

--- counter/controller.js
+++ counter/controller.js
@@ -3,3 +3,18 @@ const btnReset = document.getElementById('btnReset')
 const btnPlus = document.getElementById('btnPlus')
 
 updateCounter(counter)
+
+btnPlus.addEventListener('click', function () {
+    increase()
+    updateCounter(counter)
+})
+
+btnMinus.addEventListener('click', function () {
+    decrease()
+    updateCounter(counter)
+})
+
+btnReset.addEventListener('click', function () {
+    reset()
+    updateCounter(counter)
+})
--- main.js
+++ main.js
@@ -1,14 +1,4 @@
  input.couter = counter
 
-btnPlus.addEventListener('click', function () {
-    counter = counter + 1
-    input.value = counter
-})
-btnMinus.addEventListener('click', function () {
-    counter = counter -1
-    input.value = counter
-})
-btnReset.addEventListener('click', function () {
-    counter = 0
-    input.value = counter
-})
+
+
